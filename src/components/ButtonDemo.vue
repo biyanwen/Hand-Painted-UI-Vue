8dfddad91172b80e5c77b24e095afbcad7ac923d
biyanwen <biyw@tsintergy.com>
2021-07-24 17:08:31 +0800

    完成 button 组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,19 +1,59 @@
 <template>
   <div class="buttonDemo">
-    <Button
-        @click="test"
-        @focus="test"
-        @mouseover="test"
-    >default button
-    </Button>
-    <Button theme="primary">primary button
-    </Button>
-    <Button theme="dashed">dashed button
-    </Button>
-    <Button theme="text">text button
-    </Button>
-    <Button theme="link">link button
-    </Button>
+    <h1>示例一</h1>
+    <div>
+      <Button>default button</Button>
+      <Button theme="primary">primary button
+      </Button>
+      <Button theme="dashed">dashed button
+      </Button>
+      <Button theme="text">text button
+      </Button>
+      <Button theme="link">link button
+      </Button>
+    </div>
+    <h1>示例二</h1>
+    <div>
+      <Button level="danger">danger button
+      </Button>
+      <Button level="warning">warning button
+      </Button>
+      <Button level="success">success button
+      </Button>
+    </div>
+    <h1>示例三</h1>
+    <div>
+      <Button level="success" size="small">small
+      </Button>
+      <Button level="success">default
+      </Button>
+      <Button level="success" size="big">big
+      </Button>
+    </div>
+    <h1>示例四</h1>
+    <div>
+      <Button disabled>default button</Button>
+      <Button theme="primary" disabled>primary button
+      </Button>
+      <Button theme="dashed" disabled>dashed button
+      </Button>
+      <Button theme="text" disabled>text button
+      </Button>
+      <Button theme="link" disabled>link button
+      </Button>
+    </div>
+    <h1>示例五</h1>
+    <div>
+      <Button loading>default button</Button>
+      <Button theme="primary" loading>primary button
+      </Button>
+      <Button theme="dashed" loading>dashed button
+      </Button>
+      <Button theme="text" loading>text button
+      </Button>
+      <Button theme="link" loading>link button
+      </Button>
+    </div>
   </div>
 </template>
 
@@ -23,18 +63,13 @@ import Button from '../lib/Button.vue'
 export default {
   name: "ButtonDemo",
   components: {Button},
-  setup() {
-    const test = () => {
-      console.log("999")
-    }
-    return {test}
-  }
 }
 </script>
 
 <style lang="scss" scoped>
 .buttonDemo {
   display: flex;
+  flex-wrap: wrap;
   justify-content: space-between;
   height: 100vh;
 }
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,6 +1,9 @@
 <template>
   <button class="handButton"
-          :class="`hand-theme-${theme}`">
+          :class="`hand-theme-${theme}
+                   hand-level-${level}
+                   hand-size-${size}`">
+    <span v-if="loading" class="hand-loading"></span>
     <slot/>
   </button>
 </template>
@@ -9,7 +12,10 @@
 export default {
   name: "Button",
   props: {
-    theme: {type: String, default: "default"}
+    theme: {type: String, default: "default"},
+    level: {type: String, default: "default"},
+    size: {type: String, default: "default"},
+    loading: {type: Boolean, default: false}
   }
 }
 </script>
@@ -20,11 +26,63 @@ export default {
   border-radius: 10px 50px 10px 50px/ 50px 10px 50px 10px;
   background-color: #fff;
   font-family: 'HandPainted', serif;
+  padding: 10px;
   font-size: 18px;
-  min-width: 80px;
-  height: 30px;
+  min-width: 50px;
+  height: 40px;
   opacity: 1;
   transition: 0.5s;
+}
+
+.hand-loading {
+  width: 14px;
+  height: 14px;
+  display: inline-block;
+  margin-right: 4px;
+  border-radius: 8px;
+  border: 2px dashed;
+  animation: hand-loading-action 1s infinite linear;
+
+}
+
+@keyframes hand-loading-action {
+  0% {
+    transform: rotate(0deg)
+  }
+  100% {
+    transform: rotate(360deg)
+  }
+}
+
+.hand-size-big {
+  font-size: 28px;
+  height: 58px;
+  padding: 0 16px;
+
+}
+
+.hand-size-small {
+  font-size: 12px;
+  height: 20px;
+  padding: 0 4px;
+}
+
+.hand-level-success {
+  background-color: #d0dbc2;
+  color: #6c844d;
+  border: solid #86a361;
+}
+
+.hand-level-warning {
+  background-color: #f5f0c6;
+  color: #cab925;
+  border: solid #ddcd45;
+}
+
+.hand-level-danger {
+  background-color: #f0cbc9;
+  border: solid #a7342d;
+  color: #7f2722;
 }
 
 .hand-theme-link {
@@ -57,6 +115,11 @@ export default {
   &:hover {
     opacity: 0.4;
   }
+}
+
+[class ^="hand"][disabled] {
+  cursor: not-allowed;
+  opacity: 0.7;
 }
 
 .hand-theme-primary {
