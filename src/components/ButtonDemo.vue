4582734b679551e2bab05b22ddd55ee0d3f12064
biyanwen <biyw@tsintergy.com>
2021-07-24 13:45:19 +0800

    完成 button theme css

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,15 +1,41 @@
 <template>
-  <div>
-    这里是 button demo
+  <div class="buttonDemo">
+    <Button
+        @click="test"
+        @focus="test"
+        @mouseover="test"
+    >default button
+    </Button>
+    <Button theme="primary">primary button
+    </Button>
+    <Button theme="dashed">dashed button
+    </Button>
+    <Button theme="text">text button
+    </Button>
+    <Button theme="link">link button
+    </Button>
   </div>
 </template>
 
 <script lang="ts">
+import Button from '../lib/Button.vue'
+
 export default {
-  name: "ButtonDemo"
+  name: "ButtonDemo",
+  components: {Button},
+  setup() {
+    const test = () => {
+      console.log("999")
+    }
+    return {test}
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.buttonDemo {
+  display: flex;
+  justify-content: space-between;
+  height: 100vh;
+}
 </style>
---
+++ src/lib/Button.vue
@@ -0,0 +1,72 @@
+<template>
+  <button class="handButton"
+          :class="`hand-theme-${theme}`">
+    <slot/>
+  </button>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Button",
+  props: {
+    theme: {type: String, default: "default"}
+  }
+}
+</script>
+
+<style lang="scss">
+.handButton {
+  border: 2px solid #747272;
+  border-radius: 10px 50px 10px 50px/ 50px 10px 50px 10px;
+  background-color: #fff;
+  font-family: 'HandPainted', serif;
+  font-size: 18px;
+  min-width: 80px;
+  height: 30px;
+  opacity: 1;
+  transition: 0.5s;
+}
+
+.hand-theme-link {
+  color: #40adff;
+  border: none;
+
+  &:hover {
+    opacity: 0.7;
+  }
+}
+
+.hand-theme-text {
+  border: none;
+
+  &:hover {
+    background-color: #efecec;
+  }
+}
+
+.hand-theme-dashed {
+  border: 2px dashed #747272;
+
+  &:hover {
+    opacity: 0.4;
+  }
+}
+
+.hand-theme-default {
+
+  &:hover {
+    opacity: 0.4;
+  }
+}
+
+.hand-theme-primary {
+  background-color: #1890ff;
+  color: white;
+  border: 2px solid #0d89fa;
+
+  &:hover {
+    opacity: 0.5;
+  }
+}
+
+</style>
