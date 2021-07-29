dc9c39c3c8731838d39b3f23bb6d0a5baf6af920
biyanwen <biyw@tsintergy.com>
2021-07-29 22:20:25 +0800

    完成首页响应式编程

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <ToPBar/>
+    <!--    <ToPBar/>-->
     <div class="homePage">
       <div class="vuePng">
         <img src="../assets/png/Vue.png" alt="vue"/>
@@ -70,7 +70,7 @@ export default {
     width: 90%;
     top: 500px;
     display: flex;
-    justify-content: space-between; ;
+    justify-content: space-between;;
 
     > .tsDiv {
       display: flex;
@@ -124,20 +124,20 @@ export default {
 
 
   > .getStart {
-    top: 342px;
+    top: 312px;
     left: 750px;
     position: absolute;
   }
 
   > .github {
-    top: 342px;
+    top: 312px;
     left: 600px;
     position: absolute;
   }
 
   > .des {
     position: absolute;
-    top: 169px;
+    top: 129px;
     font-size: 65px;
     max-width: 50vw;
     left: 500px;
@@ -145,12 +145,118 @@ export default {
 
   > .vuePng {
     width: 230px;
-    left: 250px;
-    margin-top: 150px;
+    margin-top: 120px;
     position: absolute;
 
     > img {
       width: 100%;
+    }
+  }
+}
+
+@media (min-width: 1201px) {
+  .vuePng {
+    left: 210px;
+  }
+}
+
+@media (max-width: 1200px) {
+  $pageTop: 100px;
+  $pageLeft: 33%;
+  .homePage {
+    height: 90vh;
+    display: flex;
+    justify-content: center;
+
+    .featureDiv {
+      position: absolute;
+      top: $pageTop + 650px;
+      width: 100%;
+      height: 70%;
+      display: flex;
+      flex-flow: column;
+      justify-content: space-between;
+      align-items: center;
+
+      > .tsDiv {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        width: 300px;
+        margin-bottom: 25px;
+
+        > .ts {
+          font-size: 40px;
+        }
+
+        > .tsContext {
+          font-size: 20px;
+          margin-top: 25px;
+        }
+      }
+
+      > .versatileDiv {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        width: 300px;
+
+        > .versatile {
+          font-size: 40px;
+        }
+
+        > .versatileContext {
+          font-size: 20px;
+          margin-top: 25px;
+        }
+      }
+
+      > .approachableDiv {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        width: 300px;
+
+        > .approachable {
+          font-size: 40px;
+        }
+
+        > .approachableContext {
+          font-size: 20px;
+          margin-top: 25px;
+        }
+      }
+
+    }
+
+
+    > .getStart {
+      top: $pageTop +  562px;
+      left: $pageLeft - 5%;
+      position: absolute;
+    }
+
+    > .github {
+      top: $pageTop +  562px;
+      left: $pageLeft + 25%;
+      position: absolute;
+    }
+
+    > .des {
+      position: absolute;
+      top: $pageTop + 280px;
+      font-size: 65px;
+      max-width: 70vw;
+      left: $pageLeft - 20%;
+    }
+
+    > .vuePng {
+      width: 230px;
+      margin-top: $pageTop;
+
+      > img {
+        width: 100%;
+      }
     }
   }
 }
